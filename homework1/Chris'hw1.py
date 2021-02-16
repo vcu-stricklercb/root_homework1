b868e94a048937b991a60f7f8fdc02ca1d11fc4f
vcu-stricklercb <78453263+vcu-stricklercb@users.noreply.github.com>
2021-02-15 23:15:32 -0500

    Add files via upload

---
+++ homework1/Chris'hw1.py
@@ -0,0 +1,42 @@
+""" This set of functions should return the appropriate data type"""
+
+
+def return_number_3():
+    """ This function should return an integer with the value of 3"""
+
+    return_value = 3
+    return return_value
+print(return_number_3())
+
+def return_string_vcu():
+    """ This function should return a string with the lowercase value of vcu"""
+    
+    return_value = "VCU "
+    return return_value.lower()
+print(return_string_vcu())
+
+def return_lowercased_string(input_string):
+    """You have a variable called input_string that is of type string.
+    Return it but the lowercase version of it."""
+
+    return_value = input_string
+    return return_value.lower()
+print(return_lowercased_string)
+
+
+def return_without_starting_ending_whitespace(input_string):
+    """You have a variable called input_string that is of type string.
+    Return it but with the surrounding (left and right) whitespace stripped."""
+
+    return_value = input_string
+    return return_value.strip()
+print(return_without_starting_ending_whitespace)
+
+
+def return_addition(first_number, second_number):
+    """ Return the two numbers added together. """
+
+    return_value = first_number + second_number
+    return return_value
+print(return_addition())
+
